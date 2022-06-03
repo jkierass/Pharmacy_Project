17087f91fab814e48558cae5ba2604f4c7923129
jkierasi <01168887@pw.edu.pl>
2022-06-03 18:13:11 +0200

    final fix pharmacist medicine choosing

--- main.cpp
+++ main.cpp
@@ -15,15 +15,6 @@ int main()
 	Database_meds_reader database_reader_file(path);
 	MDatabase pharmacist_knowledge;
 	pharmacist_knowledge = database_reader_file.read_database();
-	//my_database.print_all_data();
-
-	std::vector<std::string> patient_symptoms = { "after_stroke", "dry_cough", "wet_cough"};
-	std::vector<Medicine> medicines;
-	Pharmacist John(121);
-	medicines = John.choose_medicines(patient_symptoms, pharmacist_knowledge);
-	for(int i = 0; i<medicines.size(); i++)
-	{
-		std::cout << medicines[i] << std::endl;
-	}
+	
 	return 0;
 }
